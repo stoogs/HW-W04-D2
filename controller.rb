7345517a4b7fb99e2dc8bd160a01f920c0e0d992
Stuart Black <stuartblack@gmail.com>
2018-07-17 21:51:47 +0100

    homepage and game page seperated

--- controller.rb
+++ controller.rb
@@ -2,6 +2,9 @@ require('sinatra')
 require('sinatra/contrib/all') if development?
 require_relative('./models/game')
 
+get '/home' do
+erb :home  # file called result.erb in /views
+end
 
 get '/hand/:hand1/:hand2' do
   hand = Game.new( params[:hand1], params[:hand2])
--- models/game.rb
+++ models/game.rb
@@ -4,30 +4,28 @@ class Game
     @hand2 = hand2
   end
 
-  win_states = {
-    "scissors" => "paper"
-  }
+  # win_states = {
+  #   "scissors" => "paper"
+  # }
 
   def result
     if @hand1 ==  @hand2
       return "You's both LOST! #{@hand1} DRAWS with #{@hand2}"
     elsif @hand1 == "rock" && @hand2 == "paper"
-      return "#{@hand2} wins"
+      return "#{@hand2} wraps #{@hand1}, player 2 wins"
     elsif @hand1 == "rock" && @hand2 == "scissors"
       return "#{@hand1} wins"
     elsif @hand1 == "paper" && @hand2 == "rock"
-      return "#{@hand1} wins"
+      return "#{@hand1} wraps #{@hand2}, player 1 wins"
     elsif @hand1 == "paper" && @hand2 == "scissors"
       return "#{@hand2} wins"
     elsif @hand1 == "scissors" && @hand2 == "paper"
       return "#{@hand1} wins"
     elsif @hand1 == "scissors" && @hand2 == "rock"
       return "#{@hand2} wins"
-    else "Read the rules"
-
-    end
-
-end
-
-
-end
+    elsif @hand1 == "hand1" && @hand2 == "hand2"
+      return "OK. Keep it fair. One hand behind your back. Please input your hands above..."  
+    else "Read the rules guys, it's 3 moves. rock, paper, scissors"
+    end # if end
+end # def result end
+end # Class end
